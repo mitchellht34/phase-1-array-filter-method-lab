23369fdf66dba2c8140d3ec7fce7b5742f771fba
Mitch Toran <mitchellht34@gmail.com>
2024-05-22 11:28:31 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name){
+    const matching = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+    return matching;
+}
+
+function fuzzyMatch(drivers, letters){
+    const matching = drivers.filter(driver => driver.indexOf(letters) === 0);
+    return matching;
+}
+
+function matchName(drivers, name){
+    const matching = drivers.filter(driver => driver.name === name);
+    return matching;
+}
